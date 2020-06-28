41d5e858830eede044619452ba44928278573356
slavabily <slavabily@me.com>
2020-06-28 23:26:05 +0300

    Understanding frames and coordinates inside GeometryReader

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -8,40 +8,41 @@
 
 import SwiftUI
 
-extension VerticalAlignment {
-    struct MidAccountAndName: AlignmentID {
-        static func defaultValue(in d: ViewDimensions) -> CGFloat {
-            d[.top]
+struct OuterView: View {
+    var body: some View {
+        VStack {
+            Text("Top")
+            InnerView()
+                .background(Color.green)
+            Text("Bottom")
         }
     }
-    
-    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
+}
+
+struct InnerView: View {
+    var body: some View {
+        HStack {
+            Text("Left")
+            GeometryReader { geo in
+                Text("Center")
+                    .background(Color.blue)
+                    .onTapGesture {
+                        print("Global center: \(geo.frame(in: .global).midX) x  \(geo.frame(in: .global).midY)")
+                        print("Custom center: \(geo.frame(in: .named("Custom")).midX) x \(geo.frame(in: .named("Custom")).midY)")
+                        print("Local center: \(geo.frame(in: .local).midX) x \(geo.frame(in: .local).midY)")
+                }
+            }
+            .background(Color.orange)
+            Text("Right")
+        }
+    }
 }
 
 struct ContentView: View {
     var body: some View {
-        HStack(alignment: .midAccountAndName) {
-            VStack {
-                Text("@twostaws")
-                    .alignmentGuide(.midAccountAndName) { (d) -> CGFloat in
-                        d[VerticalAlignment.center]
-                }
-                Text("jkhkjhlkjhl")
-                Image("apollo10")
-                .resizable()
-                    .frame(width: 64, height: 64)
-            }
-            
-            VStack {
-                Text("fhgfjhgfjhgf")
-                Text("Full name:")
-                Text("PAUL HUDSON")
-                    .alignmentGuide(.midAccountAndName, computeValue: { (d) -> CGFloat in
-                        d[VerticalAlignment.center]
-                    })
-                    .font(.largeTitle)
-            }
-        }
+        OuterView()
+            .background(Color.red)
+            .coordinateSpace(name: "Custom")
     }
 }
 
